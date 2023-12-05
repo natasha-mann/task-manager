8ac519794d22a2c132701da95ef9b67638e9c4c0
NatashaMannAND <natasha.mann@and.digital>
2023-12-05 14:43:40 +0000

    further react setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,5 @@
-import React from "react";
-import * as ReactDom from "react-dom/client";
-
 function App() {
   return <div>Task Manager</div>;
 }
 
-ReactDom.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export default App;
