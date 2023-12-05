1322b59246af57a7e3127d39be6d62a9554bb425
NatashaMannAND <natasha.mann@and.digital>
2023-12-05 16:15:08 +0000

    added create task controller function

--- server/controllers/createTask.controller.ts
+++ server/controllers/createTask.controller.ts
@@ -1,1 +1,24 @@
-export const createTask = () => {};
+import { Request, Response } from "express";
+
+import Task, { ITask } from "../models/task.model";
+import { IUser } from "../models/user.model";
+
+type CreateTaskPayload = {
+  title: ITask["title"];
+  details?: ITask["details"];
+  priorityLevel: ITask["priorityLevel"];
+  status: ITask["status"];
+  user: IUser["_id"];
+};
+
+export const createTask = async (req: Request, res: Response) => {
+  try {
+    const data: CreateTaskPayload = req.body;
+    const newTask = await Task.create(data);
+    return res
+      .status(201)
+      .send({ message: "Task successfully created", data: newTask });
+  } catch (error) {
+    return res.status(500).send({ message: "Failed to create task", error });
+  }
+};
